e92213be8b0d7c3da56b4b8696cbc38456c146c6
Awierdo-Hacks <overload.am@hotmail.com>
2023-03-07 11:11:41 +0100

    login style

--- GIPCOIN/login.php
+++ GIPCOIN/login.php
@@ -1,34 +1,76 @@
-<html>
-  <head>
-    <title>Login</title>
-    <link rel="stylesheet" href="style_login.css">
-  </head>
-  <body>
-    <div class="container">
-      <form action="login.php" method="post" class="form-group">
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username" class="form-control">
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" class="form-control">
-        <br>
-        <input type="submit" value="Login" class="btn btn-danger">
-      </form>
-    </div>
-  </body>
-</html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+  <title>Login</title>
+  <link rel="stylesheet" href="style_login.css">
+</head>
+<body>
+  
+	<a href="#" class="logo" target="_blank">
+		<img src="image\logofulltrans.png" alt="">
+	</a>
 
-<?php
-   session_start();
-   if($_SERVER["REQUEST_METHOD"] == "POST"){
-       $username = $_POST["username"];
-       $password = $_POST["password"];
-       if ($username == "bankier" && $password == "geld") {
-           $_SESSION["logged_in"] = true;
-           header("Location: controle_paneel.php");
-       } else {
-           $_SESSION["logged_in"] = false;
-           echo "Incorrect username or password. Please try again.";
-       }
-   }
-?>
-    
+	<div class="section">
+		<div class="container">
+			<div class="row full-height justify-content-center">
+				<div class="col-12 text-center align-self-center py-5">
+					<div class="section pb-5 pt-5 pt-sm-2 text-center">
+						<h6 class="mb-0 pb-3"><span>Log In </span><span>Sign Up</span></h6>
+			          	<input class="checkbox" type="checkbox" id="reg-log" name="reg-log"/>
+			          	<label for="reg-log"></label>
+						<div class="card-3d-wrap mx-auto">
+							<div class="card-3d-wrapper">
+								<div class="card-front">
+									<div class="center-wrap">
+										<div class="section text-center">
+											<h4 class="mb-4 pb-3">Log In</h4>
+											<div class="form-group">
+											<form action="" method="post">
+                      <input type="email" name="logemail" class="form-style" placeholder="Your Email" id="logemail" autocomplete="off" required>
+												<i class="input-icon uil uil-at"></i>
+											</div>	
+											<div class="form-group mt-2">
+												<input type="password" name="logpass" class="form-style" placeholder="Your Password" id="logpass" autocomplete="off" required >
+												<i class="input-icon uil uil-lock-alt"></i>
+											</div>
+											<input type="submit" value="inloggen" class="btn mt-4"> 
+                            			
+				      					</div>
+			      					</div>
+			      				</div>
+                    </form>	
+								<div class="card-back">
+									<div class="center-wrap">
+										<div class="section text-center">
+											<h4 class="mb-4 pb-3">Sign Up</h4>
+											<form action="" method="post">
+                      <div class="form-group">
+												<input type="text" name="logname" class="form-style" placeholder="Uw volledige naam" id="logname" autocomplete="off"required>
+												<i class="input-icon uil uil-user"></i>
+											</div>	
+											<div class="form-group mt-2">
+												<input type="email" name="logemail" class="form-style" placeholder="Uw Email" id="logemail" autocomplete="off"required>
+												<i class="input-icon uil uil-at"></i>
+											</div>	
+											<div class="form-group mt-2">
+												<input type="password" name="logpass" class="form-style" placeholder="Uw wachtwoord" id="logpass" autocomplete="off"required>
+												<i class="input-icon uil uil-lock-alt"></i>
+											</div>
+                      <input type="submit" value="Registreer" class="btn mt-4">                     
+                      </form>
+				      					</div>
+			      					</div>
+			      				</div>
+			      			</div>
+			      		</div>
+			      	</div>
+		      	</div>
+	      	</div>
+	    </div>
+	</div>
+</body>
+</html>
